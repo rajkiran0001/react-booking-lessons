3e8408fc0516e4ec27352d046368c4fe6db4dffe
Rajkiran Gurunathan (TA-14) <Rajkiran.GA.Gurunathan@bmw.de>
2020-03-31 21:33:31 +0200

    comments

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,61 +1,58 @@
 import React, { Component } from 'react'
 
 const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
   }
 
-class ContactForm extends Component {
-    constructor(props) {
-        super(props)
+  /* Here’s the juicy bit for posting the form submission */
 
-        this.state = {
-            name: "", email: "", message: ""   
-        }
-    }
-        /* Here’s the juicy bit for posting the form submission */
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
 
-        handleSubmit = e => {
-            fetch("/", {
-              method: "POST",
-              headers: { "Content-Type": "application/x-www-form-urlencoded" },
-              body: encode({ "form-name": "contact", ...this.state })
-            })
-              .then(() => alert("Success!"))
-              .catch(error => alert(error));
-      
-            e.preventDefault();
-          };
-      
-          handleChange = e => this.setState({ [e.target.name]: e.target.value });
+    e.preventDefault();
+  };
 
-          render() {
-            const { name, email, message } = this.state;
-            return (
-              <form onSubmit={this.handleSubmit} name="contact"  data-netlify="true" data-netlify-honeypot="bot-field">
-                  <input type="hidden" name="form-name" value="contact" />
-                <p>
-                  <label>
-                    Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Message: <textarea name="message" value={message} onChange={this.handleChange} />
-                  </label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
-            );
-          }
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    );
+  }
 }
 
 export default ContactForm
